d2d843f31a65f7867591090ec078e8ca97a8b73e
Marcel Rösler <marcel.roesler@helmundwalter.de>
2021-09-25 22:25:38 +0200

    go on with the presentation

---
+++ components/Centered.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const Centered = styled.div`
+  align-self: center;
+  justify-self: center;
+  grid-row: 1/2;
+  grid-column: 1/2;
+`;
+
+export default Centered
--- components/YoutubeVideo.js
+++ components/YoutubeVideo.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import styled from 'styled-components';
+
+import Centered from "./Centered";
+
 import "./YoutubeVideo.css"
 
 const VideoWrapper = styled.div`
   display: grid;
-`;
-
-const Centered = styled.div`
-  align-self: center;
-  justify-self: center;
-  grid-row: 1/2;
-  grid-column: 1/2;
 `;
 
 const YoutubeVideo = ({
